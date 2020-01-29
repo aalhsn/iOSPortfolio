50baeb7a986a12bbe217588cd8c298a93f3d896c
Abdullah Alhasan <a.a.alhasan95@gmail.com>
2020-01-29 18:05:52 +0300

    first commit

--- myPortifolioIOS/ViewController.swift
+++ myPortifolioIOS/ViewController.swift
@@ -14,6 +14,13 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+    self.navigationController?.navigationBar.isHidden = false  }
+    
+    override func viewWillAppear(_ animated: Bool) {
+    self.navigationController?.navigationBar.isHidden = true  }
+
 
 
 }
