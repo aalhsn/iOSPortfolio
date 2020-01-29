6577cf4c978f1e06741360ec571ee004bd22a8c4
Abdullah Alhasan <a.a.alhasan95@gmail.com>
2020-01-29 18:04:22 +0300

    first commit

---
+++ myPortifolioIOS/AppListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AppListViewController.swift
+//  myPortifolioIOS
+//
+//  Created by Abdullah Alhasan on 1/29/20.
+//  Copyright © 2020 Abdullah. All rights reserved.
+//
+
+import UIKit
+
+class AppListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
